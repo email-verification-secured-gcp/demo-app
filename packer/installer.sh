18a52886c2ebe8c73f1438b6d995a075addb836f
shobaraajeswar <shoban@Shobaraajeswars-MacBook-Air.local>
2024-02-20 23:30:09 -0500

    int test

--- packer/installer.sh
+++ packer/installer.sh
@@ -8,9 +8,11 @@ sudo dnf -y install nodejs
 sudo dnf -y install mysql-server
 sudo systemctl enable mysqld
 sudo systemctl start mysqld
-sudo mysqladmin -u root password "${DB_PASSWORD}"
+sudo mysqladmin -u root password "$DB_PASSWORD"
 
-mysql -u root -p"${DB_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS cloud;"
+mysql -u root -p"$DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS cloud;"
+mysql -u root -p"$DB_PASSWORD" -e "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}' ";
+mysql -u root -p"$DB_PASSWORD" -e "GRANT ALL PRIVILEGES ON cloud.* TO '${DB_USER}'@'localhost'";
 
 # Install unzip
 sudo dnf -y install unzip
