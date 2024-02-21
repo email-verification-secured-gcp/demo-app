63f679c57517cf67efaa8ce714a4f63edc4653a8
shobaraajeswar <shoban@Shobaraajeswars-MacBook-Air.local>
2024-02-21 14:55:28 -0500

    on push main

--- packer/startserver.sh
+++ packer/startserver.sh
@@ -10,14 +10,3 @@ sudo systemctl start node
 sudo systemctl restart node
 sudo systemctl status node
 
-APISRVC=$?
-
-if [ $APISRVC -eq 0 ]; then
-
-  echo "web app started successfully!"
-
-else
-
-  echo "Unable to start web app"
-
-fi
