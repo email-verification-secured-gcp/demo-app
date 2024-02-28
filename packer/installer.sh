b125352e1debf5fff3f36325a015f5114395da9d
shobaraajeswar <shoban@Shobaraajeswars-MacBook-Air.local>
2024-02-28 16:49:35 -0500

    remove db installation

--- packer/installer.sh
+++ packer/installer.sh
@@ -4,14 +4,5 @@ sudo dnf -y update
 curl -sL https://rpm.nodesource.com/setup_20.x | sudo bash -
 sudo dnf -y install nodejs
 
-# Install MySQL server
-sudo dnf -y install mysql-server
-sudo systemctl enable mysqld
-sudo systemctl start mysqld
-
-mysql -u root  -e "CREATE DATABASE IF NOT EXISTS cloud;"
-mysql -u root  -e "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}' ";
-mysql -u root  -e "GRANT ALL PRIVILEGES ON cloud.* TO '${DB_USER}'@'localhost'";
-
 # Install unzip
 sudo dnf -y install unzip
