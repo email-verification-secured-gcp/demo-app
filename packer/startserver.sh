53f18e990eb498983b5d47945ece0536c0b005d5
shobaraajeswar <shoban@Shobaraajeswars-MacBook-Air.local>
2024-02-28 15:31:47 -0500

    remove status check

--- packer/startserver.sh
+++ packer/startserver.sh
@@ -9,15 +9,3 @@ sudo systemctl enable node
 sudo systemctl start node
 sudo systemctl restart node
 sudo systemctl status node
-
-APISRVC=$?
-
-if [ $APISRVC -eq 0 ]; then
-
-  echo "web app started successfully!"
-
-else
-
-  echo "Unable to start web app"
-
-fi
