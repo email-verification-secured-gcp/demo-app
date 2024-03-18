1c6c431a0b495e55f37f8bca63c0dd1e73767605
shobaraajeswar <shoban@Shobaraajeswars-MacBook-Air.local>
2024-03-18 15:04:52 -0400

    ops-agent

--- packer/installer.sh
+++ packer/installer.sh
@@ -6,3 +6,7 @@ sudo dnf -y install nodejs
 
 # Install unzip
 sudo dnf -y install unzip
+
+#Install ops agent
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
