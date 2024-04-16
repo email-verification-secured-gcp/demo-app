ab32bf3d54220ecd67ca26fc73488661f3333c85
shobaraajeswar <shoban@Shobaraajeswars-MacBook-Air.local>
2024-04-16 18:02:09 -0400

    v2

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,6 @@ import userRouter from './user-route.js';
 import veirfyRouter from "./verify-route.js";
 export default (app) => {
     app.use('/healthz',healthRouter);
-    app.use('/v1/user',userRouter);
+    app.use('/v2/user',userRouter);
     app.use('/verify-email',veirfyRouter);
 }   
